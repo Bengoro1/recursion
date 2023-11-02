22723a7fbfeb7ecea94fe1e02e598a728446bfe5
Bengoro <127534872+Bengoro1@users.noreply.github.com>
2023-11-02 22:02:01 +0100

    merge sort function

---
+++ merge-sort.js
@@ -0,0 +1,38 @@
+const mergeSort = (arr) => {
+  if (!Array.isArray(arr)) return 'Invalid format';
+  if (arr.length === 1) return arr;
+  const m = Math.floor(arr.length / 2);
+  const l = arr.slice(0, m);
+  const r = arr.slice(m, arr.length);
+  console.log(l, r);
+  return merge(mergeSort(l), mergeSort(r));
+}
+
+const merge = (l, r) => {
+  const result = [];
+  let i = 0;
+  let j = 0;
+  while (i < l.length && j < r.length) { 
+    if (l[i] < r[j]) {
+      result.push(l[i]);
+      i++;
+    } else {
+      result.push(r[j]);
+      j++;
+    }
+  }
+
+  while (i < l.length) {
+    result.push(l[i]);
+    i++;
+  } 
+
+  while (j < r.length) {
+    result.push(r[j]);
+    j++;
+  }
+
+  return result;
+}
+const a2 = [7, 14, 48, 25, 17, 9];
+console.log(mergeSort(a2));
